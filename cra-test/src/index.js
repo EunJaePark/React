5347aa9857bef73abc2df98083a47fa95b8bc55e
EunJaePark <design0728@naver.com>
2020-12-08 20:36:44 +0900

    update 1_리액트 프로젝트 시작하기

--- cra-test/src/index.js
+++ cra-test/src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Box from './Box';
+import Button from './Button';
 
 ReactDOM.render(
-  // StrictMode는 개발환경에서 동작하는데, 리액트에서 잘못 사용하는 것에 대해 잡아내기 위해 사용하는 것이다. 
-  <React.StrictMode> 
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    <Button size="big" />
+    <Button size="small" />
+    <Box size="big" />
+    <Box size="small" />
+  </div>,
+  document.getElementById('root'),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// ReactDOM.render(
+//   // StrictMode는 개발환경에서 동작하는데, 리액트에서 잘못 사용하는 것에 대해 잡아내기 위해 사용하는 것이다. 
+//   <React.StrictMode> 
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
